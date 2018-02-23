62c91592de5fa81c15b1e090ed5058072e8c7f79
artagel <dan@syssec.biz>
2018-02-23 12:37:43 +0400

    Added more info to spider stats

--- api_SpiderStats.py
+++ api_SpiderStats.py
@@ -8,12 +8,14 @@ import textwrap
 import requests
 import json
 import urllib.parse
-
-api_url = 'http://192.168.163.133/api/'
+import configparser
+import sys
+import os
 
 
 def count_field(endpoint, query):
-    r = requests.get(api_url + endpoint + '?results_per_page=1&q=' + urllib.parse.quote_plus(json.dumps(query)), verify=False)
+    r = requests.get(api_url + endpoint + '?results_per_page=1&q=' + urllib.parse.quote_plus(json.dumps(query)),
+                     verify=False)
     if r.status_code == 200:
         # If correct then it returns the object data
         return json.loads(r.text).get('num_results')
@@ -21,48 +23,72 @@ def count_field(endpoint, query):
         return {}
 
 
-print('Gathering metrics...')
+if __name__ == '__main__':
 
-print('Url count...')
-empty_query = {"filters": []}
-url_count = count_field('urls', empty_query)
-date_query = {
-    "filters": [
-        {
-            "op": "ne",
-            "name": "date",
-            "val": "1900-01-01"
-        }
-    ]}
-date_query = {"filters": [{"op": "ne","name": "date","val": "1900-01-01"}]}
-url_count_scanned = count_field('urls', date_query)
-url_count_percentage = (url_count_scanned / url_count)
+    # Load the configuration file.
+    try:
+        config = configparser.ConfigParser()
+        config.read('spider.cfg')
+        api_url = config['API'].get('api_url')
+    except Exception as e:
+        print('Could not parse spider.cfg. Please verify its syntax.')
+        sys.exit(0)
 
-print('Onions...')
-onion_query = {
-    "filters": [
-        {
-            "op": "ne",
-            "name": "last_online",
-            "val": "1900-01-01"
-        },
-        {
-            "op": "eq",
-            "name": "online",
-            "val": "true"
-        }
-    ]}
-onion_count = count_field('onions', onion_query)
+    print('Gathering metrics...')
 
-print('–' * 70)
-print('Results:')
-messages = [
-    'So far, TorSpider has scanned {:,} ({:.2%}) of the {:,} urls it has',
-    'discovered. Of the scanned sites, {:,} are known to be active.'
-]
+    print('Url count...')
+    empty_query = {"filters": []}
+    url_count = count_field('urls', empty_query)
+    date_query = {
+        "filters": [
+            {
+                "op": "ne",
+                "name": "date",
+                "val": "1900-01-01"
+            }
+        ]}
+    date_query = {"filters": [{"op": "ne", "name": "date", "val": "1900-01-01"}]}
+    url_count_scanned = count_field('urls', date_query)
+    url_count_percentage = (url_count_scanned / url_count)
 
-message = ' '.join(messages)
-message = message.format(url_count_scanned, url_count_percentage,
-                         url_count, onion_count)
-message = textwrap.fill(message)
-print(message)
+    print('Onions...')
+    onion_query = {
+        "filters": [
+            {
+                "op": "ne",
+                "name": "last_online",
+                "val": "1900-01-01"
+            },
+            {
+                "op": "eq",
+                "name": "online",
+                "val": "true"
+            }
+        ]}
+    onion_count = count_field('onions', onion_query)
+
+    # Get the totals
+    total_onions = count_field('onions', empty_query)
+    total_urls = count_field('urls', empty_query)
+    total_pages = count_field('pages', empty_query)
+    total_forms = count_field('forms', empty_query)
+    total_links = count_field('links', empty_query)
+
+    print('–' * 70)
+    print('Results:')
+    messages = [
+        'So far, TorSpider has scanned {:,} ({:.2%}) of the {:,} urls it has',
+        'discovered. Of the scanned sites, {:,} are known to be active.' + os.linesep,
+        'Table Stats:' + os.linesep,
+        'Total Onions: {:}' + os.linesep,
+        'Total Urls: {:}' + os.linesep,
+        'Total Pages: {:}' + os.linesep,
+        'Total Forms: {:}' + os.linesep,
+        'Total Links: {:}'
+    ]
+
+    message = ' '.join(messages)
+    message = message.format(url_count_scanned, url_count_percentage,
+                             url_count, onion_count, total_onions, total_urls, total_pages, total_forms, total_links)
+
+    print(message)
